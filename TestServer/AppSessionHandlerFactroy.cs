2745cd8753e7925f9894ee71f5520877da81369f
JeongTaeLee <jeongtae8633@gmail.com>
2021-02-11 17:48:55 +0900

    vs git test

--- TestServer/AppSessionHandlerFactroy.cs
+++
@@ -1,9 +0,0 @@
-﻿using SocketEngine.Servers.Handlers;
-
-namespace TestServer
-{
-    class AppSessionHandlerFactroy : ISessionHandlerFactory
-    {
-        public BaseSessionHandler CreateSession() => new AppSessionHandler();
-    }
-}
--- TestServer/Program.cs
+++ TestServer/Program.cs
@@ -1,7 +1,5 @@
 ﻿using NLog;
 using SocketEngine.Logging;
-using SocketEngine.Servers.Asyncs;
-using SocketEngine.Servers.Configs;
 using System;
 
 namespace TestServer
@@ -10,34 +8,7 @@ namespace TestServer
     {
         static void Main(string[] args)
         {
-            NLogLoggerFactory logFactroy = null;
-
-#if DEBUG
-            LogManager.Configuration = new NLog.Config.XmlLoggingConfiguration("./NLog.config");
-            logFactroy = new NLogLoggerFactory("./NLog.config");
-#else
-            LogManager.Configuration = new NLog.Config.XmlLoggingConfiguration("./NLog.config");
-            logFactroy = new NLogLoggerFactory("./NLog.config");
-#endif
-
-            ServerConfig config = new ServerConfig.Builder("127.0.0.1", 9199)
-                .SetMaxConnection(1000000)
-                .SetReceiveBufferSize(2048)
-                .SetSendBufferSize(2048)
-                .Build();
-
-
-            AsyncSocketServer asyncServer = new AsyncSocketServer(config,
-                new AppServerHandler(),
-                new AppSessionHandlerFactroy(),
-                logFactroy);
-
-            asyncServer.Start();
-
-            while (true)
-            {
-
-            }
+            Console.WriteLine("Test0");
         }
     }
 }
